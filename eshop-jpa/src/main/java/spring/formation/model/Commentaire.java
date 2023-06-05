aef5c88e84516a36eab69d8a37f9adebb5cfa74a
Eric <eric@semantik.fr>
2023-06-05 16:23:48 +0200

    eshop-jpa-v0

---
+++ eshop-jpa/src/main/java/spring/formation/model/Commentaire.java
@@ -0,0 +1,89 @@
+package spring.formation.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "commentaire")
+public class Commentaire {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "COM_ID")
+	private Long id;
+
+	@Column(name = "COM_DATE", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date = new Date();
+
+	@Column(name = "COM_NOTE", nullable = false)
+	private int note = 0;
+
+	@Column(name = "COM_COMMENTAIRE", nullable = false)
+	private String commentaire;
+
+	@ManyToOne
+	@JoinColumn(name = "COM_PRODUIT_ID")
+	private Produit produit;
+
+	@ManyToOne
+	@JoinColumn(name = "COM_CLIENT_ID")
+	private Client client;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getNote() {
+		return note;
+	}
+
+	public void setNote(int note) {
+		this.note = note;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+}
