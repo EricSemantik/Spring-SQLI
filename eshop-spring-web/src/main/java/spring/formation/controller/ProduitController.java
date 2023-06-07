39132e53f7a63dba295fac6b072067f3a9191896
Eric <eric@semantik.fr>
2023-06-07 10:32:48 +0200

    eshop-spring-web-v2

--- eshop-spring-web/src/main/java/spring/formation/controller/ProduitController.java
+++ eshop-spring-web/src/main/java/spring/formation/controller/ProduitController.java
@@ -2,6 +2,7 @@ package spring.formation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import spring.formation.repo.IProduitRepository;
 
@@ -10,5 +11,10 @@ public class ProduitController {
 	
 	@Autowired
 	private IProduitRepository produitRepository;
+	
+//	@GetMapping("/produit/list")
+//	public String list() {
+//		
+//	}
 
 }
