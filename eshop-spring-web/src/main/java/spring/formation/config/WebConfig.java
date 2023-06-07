4bbc39594f1e6a213eb8b64a1d905e9d25ced2ed
Eric <eric@semantik.fr>
2023-06-07 10:22:04 +0200

    eshop-spring-web-v1

---
+++ eshop-spring-web/src/main/java/spring/formation/config/WebConfig.java
@@ -0,0 +1,12 @@
+package spring.formation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spring.formation.controller")
+public class WebConfig {
+
+}
